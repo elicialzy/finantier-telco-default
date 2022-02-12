5a47c86d8ebeb6d2627cc732f56a3e164a660bf8
elicialzy <65524472+elicialzy@users.noreply.github.com>
2022-02-13 03:48:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,28 +18,30 @@ def predict():
     query_df = pd.DataFrame(json_)
 
     # Data Pre-processing to drop unimportant features from feature selection
-    query_df = query_df.drop(columns=['PhoneService', 'gender', 'MultipleLines', 'Dependents', 'Partner', 'SeniorCitizen'])
+    query_df = query_df.drop(columns=['PhoneService', 'gender', 'MultipleLines'])
     # Data Pre-processing to convert categorical data to numerical columns
     ohe = preprocessing.OneHotEncoder()
     arr_x_train = ohe.fit_transform(query_df[categorical_cols]).toarray()
     x_train_labels = ohe.get_feature_names_out()
     x_train_labels = np.array(x_train_labels).ravel()
     x_train = pd.DataFrame(arr_x_train, columns=x_train_labels)
-    add_x_train = query_df[['tenure', 'MonthlyCharges']].reset_index(drop=True)
+    add_x_train = query_df[['tenure', 'MonthlyCharges', 'TotalCharges']].reset_index(drop=True)
     x_train = pd.concat([x_train, add_x_train], axis=1) 
 
     # Reindex columns based on loaded model columns
     query = x_train.reindex(columns=model_columns, fill_value=0)
+    query = query.to_numpy().astype(np.float32)
     
     # Predict results
-    prediction = model.predict(query.to_numpy())
+    prediction = model.predict(query)
 
     # Return predicted result
     return jsonify({'prediction': str(list(prediction))})
 
 
-categorical_cols = ['InternetService', 'OnlineSecurity',
-       'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+categorical_cols = ['SeniorCitizen', 'Partner', 'Dependents',
+       'InternetService', 'OnlineSecurity', 'OnlineBackup', 
+       'DeviceProtection', 'TechSupport', 'StreamingTV',
        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod']
 
 
